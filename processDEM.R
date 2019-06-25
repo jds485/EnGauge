c82640befdb5421a26f33e88348635972dcf084c
Jared D Smith <jared.d.smith485@gmail.com>
2019-06-25 13:21:09 -0400

    Function to mosiac DEM tiles together into one tile, and project into the project coordinate system. Moved here from the main script.

---
+++ processDEM.R
@@ -0,0 +1,18 @@
+#Function for DEM mosaikinng from separate tiles
+processDEM = function(dir_DEMs, #Directories for DEMs 
+                      f_DEMs,    #Filenames of DEMs in those directories
+                      pCRS      #Project coordinate system
+){
+  #Mosiac the tiles together
+  for (d in 1:length(dir_DEMs)){
+    if (d > 1){
+      DEM2 = raster(x = paste0(dir_DEMs[d], "/", f_DEMs[d]))
+      DEM = mosaic(DEM, DEM2, fun = mean)
+    }else{
+      DEM = raster(x = paste0(dir_DEMs[d], "/", f_DEMs[d]))
+    }
+  }
+  #Project to project coordinate system
+  DEM = projectRaster(DEM, crs = CRS(pCRS))
+  return(DEM)
+}
