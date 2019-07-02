951ee3ee08ed4398ff54019afb3c1da8d9d90be2
Jared D Smith <jared.d.smith485@gmail.com>
2019-07-02 09:36:36 -0400

    Made a function for selecting water quality data from a specified characteristic name and result type. This was in the main script, and is now a separate function.

--- extractWQdata.R
+++ extractWQdata.R
@@ -63,4 +63,71 @@ makeWQStationList = function(pattern, #the grep pattern to search for in the fil
   return(StationList)
 }
 
-
+selectWQDataType = function(wd,       #the directory to search for files
+                            charName, #the CharacteristicName keyword to grep for
+                            resName  #the ResultSampleFractionText keyword to grep for
+                            ){
+  od = getwd()
+  setwd(wd)
+  #get all files in working directory
+  lst = list.files()
+  #gather all files with the CharacteristicName for charName measurements only
+  cn = grep(x = lst, pattern = charName, ignore.case = TRUE)
+  #gather all files with the ResultSampleFractionText = resName
+  rn = grep(x = lst[cn], pattern = resName, ignore.case = TRUE)
+  #grepped files only
+  f_TN = lst[cn][rn]
+  
+  #Make a list of all of the gathered datasets
+  TN = list()
+  for (i in 1:length(f_TN)){
+    f = read.table(f_TN[i], header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+    
+    #Remove any NA dates. These entries are useless.
+    f = f[!is.na(f$ActivityStartDate),]
+    
+    #Add the full date and time string as available in each record.
+    # NOTE there is a field ActivityStartDateTime that already has this, but it has NAs when the time zone or the time are NA. That's undesireable.
+    f$DateTimeNoNA = str_c(f$ActivityStartDate, ' ', ifelse(is.na(f$ActivityStartTime.Time), '', f$ActivityStartTime.Time), ' ', ifelse(is.na(f$ActivityStartTime.TimeZoneCode), '', f$ActivityStartTime.TimeZoneCode))
+    
+    #Average date and time for those days with an end date and time listed
+    f$AvgDate = (as.Date(f$ActivityEndDate) - as.Date(f$ActivityStartDate))*.5 + as.Date(f$ActivityStartDate)
+    f$AvgDateTime = (as.POSIXct(f$ActivityEndDateTime) - as.POSIXct(f$ActivityStartDateTime))*.5 + as.POSIXct(f$ActivityStartDateTime)
+    
+    #Make a new column for the sort date.
+    f$SortDate = as.Date(f$ActivityStartDate)
+    f$SortDateTime = as.POSIXct(f$ActivityStartDateTime)
+    #Add any averaged dates to the sort date
+    f$SortDate[!is.na(f$AvgDate)] = as.Date(f$AvgDate[!is.na(f$AvgDate)])
+    f$SortDateTime[!is.na(f$AvgDateTime)] = as.POSIXct(f$AvgDateTime[!is.na(f$AvgDateTime)])
+    
+    #Place the measurements in chronological order by the sort date and time
+    # First order by time (which may have some NA values), then order by date (no NA values)
+    f = f[order(as.POSIXct(f$SortDateTime)),][order(f[order(as.POSIXct(f$SortDateTime)),]$SortDate),]
+    
+    #Check that the units are all the same for each measurement
+    us = unique(f$ResultMeasure.MeasureUnitCode)
+    #remove NAs if the results with NA units are all also NA
+    if(all(is.na(f$ResultMeasureValue[is.na(f$ResultMeasure.MeasureUnitCode)]))){
+      us = us[!is.na(us)]
+    }
+    if (length(us) > 1){
+      print(paste('Warning: more than one measurement unit for station ', f$MonitoringLocationIdentifier[1], '. Fix manually.'))
+    }
+    
+    #Check for detection limits
+    dl = unique(f$DetectionQuantitationLimitMeasure.MeasureValue)
+    #remove NAs
+    dl = dl[!is.na(dl)]
+    if(length(dl) != 0){
+      print(paste0('Warning: detection limits for station ', f$MonitoringLocationIdentifier[1], '. Fix manually.'))
+    }
+    
+    #add data to list
+    TN = c(TN, list(f))
+    names(TN)[length(TN)] = f$MonitoringLocationIdentifier[1]
+  }
+  
+  setwd(od)
+  return(TN)
+}
