8d1d3727799cab00721e7c2cd5e22f4e8ca79720
Jared D Smith <jared.d.smith485@gmail.com>
2019-07-09 14:03:35 -0400

    Generalized this function for use with datasets other than the WQP

--- aggregateTimeseries.R
+++ aggregateTimeseries.R
@@ -4,7 +4,10 @@
 #Fixme: This is currently only for WQP data. Make a generic aggregation field name in the function call
 
 aggregateTimesteps = function(StationList, #named list of station timeseries
-                              aggVal #text: d, m, a. Daily, Monthly, Annual
+                              aggVal, #text: d, m, a. Daily, Monthly, Annual
+                              aggVar, #variable in StationList to aggregate by
+                              date, #fieldname supplying the date
+                              site  #fieldname supplying the site ID
 ){
   ld = list()
   lm = list()
@@ -14,25 +17,29 @@ aggregateTimesteps = function(StationList, #named list of station timeseries
     if ('d' %in% aggVal){
       #Average days with multiple measurements into a daily average concentration
       #make a new file with only the values by date
-      daily = stats::aggregate(ResultMeasureValue ~ SortDate, data = StationList[[i]], FUN = mean)
-      daily$MonitoringLocationIdentifier = rep(StationList[[i]]$MonitoringLocationIdentifier[1], nrow(daily))
+      daily = stats::aggregate(StationList[[i]][,aggVar] ~ StationList[[i]][,date], data = StationList[[i]], FUN = mean)
+      colnames(daily)[colnames(daily) == 'StationList[[i]][, aggVar]'] = aggVar
+      colnames(daily)[colnames(daily) == 'StationList[[i]][, date]'] = date
+      daily[,site] = rep(StationList[[i]][1,site], nrow(daily))
       ld = c(ld, list(daily))
     }
     if ('m' %in% aggVal){
       #Month-year averaging:
-      mthyr = stats::aggregate(ResultMeasureValue ~ month(StationList[[i]]$SortDate) + year(StationList[[i]]$SortDate), data = StationList[[i]], FUN = mean)
+      mthyr = stats::aggregate(StationList[[i]][,aggVar] ~ month(StationList[[i]][,date]) + year(StationList[[i]][,date]), data = StationList[[i]], FUN = mean)
       colnames(mthyr)[1] = 'month'
       colnames(mthyr)[2] = 'year'
+      colnames(mthyr)[colnames(mthyr) == 'StationList[[i]][, aggVar]'] = aggVar
       mthyr$YrMthDy = as.Date(paste0(mthyr$year, '-', mthyr$month, '-01'))
-      mthyr$MonitoringLocationIdentifier = rep(StationList[[i]]$MonitoringLocationIdentifier[1], nrow(mthyr))
+      mthyr[,site] = rep(StationList[[i]][1,site], nrow(mthyr))
       lm = c(lm, list(mthyr))
     }
     if ('a' %in% aggVal){
       #Year averaging:
-      ann = stats::aggregate(ResultMeasureValue ~ year(StationList[[i]]$SortDate), data = StationList[[i]], FUN = mean)
+      ann = stats::aggregate(StationList[[i]][,aggVar] ~ year(StationList[[i]][,date]), data = StationList[[i]], FUN = mean)
       colnames(ann)[1] = 'year'
+      colnames(ann)[colnames(ann) == 'StationList[[i]][, aggVar]'] = aggVar
       ann$YrMthDy = as.Date(paste0(ann$year, '-01-01'))
-      ann$MonitoringLocationIdentifier = rep(StationList[[i]]$MonitoringLocationIdentifier[1], nrow(ann))
+      ann[,site] = rep(StationList[[i]][1,site], nrow(ann))
       la = c(la, list(ann))
     }
   }
